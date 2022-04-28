7ee309ae575237b8a595b6b82e693ac065c3a5a4
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 16:28:13 -0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+const path = require('path')
+const app = express()
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '0ebda9e86b104b189239a5f6d2ebca72',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.use(express.static('public'))
+const port  = process.env.PORT || 4005
+app.listen(port, function(){
+console.log(`Listening on da port that is ${port}`)
+})
+
