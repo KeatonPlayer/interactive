dcc4c4cc54546c54966151d5be8e1a3ac3d59b1d
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 17:08:49 -0600

    second commit

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,21 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
-
 app.use(express.static(path.join(__dirname, '../frontend')))
+
+
+
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error('function does not exist')
+  }
+  
+
+
+
+
 
 const port  = process.env.PORT || 4005
 app.listen(port, function(){
