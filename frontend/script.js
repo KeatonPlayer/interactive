585a9d9f178fced16eda81b89b2aee2ec13a1e8e
Keaton Player <keatonplayer18@gmail.com>
2022-04-28 16:56:42 -0600

    second commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,13 @@
-console.log('it works')
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '0ebda9e86b104b189239a5f6d2ebca72',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error('function does not exist')
+  }
+  
